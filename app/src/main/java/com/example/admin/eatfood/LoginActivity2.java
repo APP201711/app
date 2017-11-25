056e641eba79a7845fd11867e1ef200f55dc5817
img21326 <img21326@gmail.com>
2017-11-25 10:13:18 +0800

    add auth success img21326@gmail.com/123456

--- app/src/main/java/com/example/admin/eatfood/LoginActivity2.java
+++ app/src/main/java/com/example/admin/eatfood/LoginActivity2.java
@@ -6,7 +6,11 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -19,11 +23,14 @@ public class LoginActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
         auth = FirebaseAuth.getInstance();
+
         authListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(
                     @NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
+                Log.d("System Log", String.valueOf(user));
+                Log.d("System Log", "登入初始化");
                 if (user!=null) {
                     Log.d("onAuthStateChanged", "登入:"+
                             user.getUid());
@@ -46,12 +53,30 @@ public class LoginActivity2 extends AppCompatActivity {
         auth.removeAuthStateListener(authListener);
     }
     public void login(View v){
+
+
         String email = ((EditText)findViewById(R.id.email))
                 .getText().toString();
         String password = ((EditText)findViewById(R.id.password))
                 .getText().toString();
         Log.d("AUTH", email+"/"+password);
-        auth.signInWithEmailAndPassword(email, password);
+
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("login", "signInWithEmail:success");
+                            FirebaseUser user = auth.getCurrentUser();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("Login", "signInWithEmail:failure", task.getException());
+                        }
+                    }
+                });
+
     }
 }
 
